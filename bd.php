c8fc8932b6a1536668613a05c230e237db534079
Mangelxd <149787844+Mangelxd@users.noreply.github.com>
2025-05-11 20:30:32 +0200

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,34 +1,23 @@
 <?php
-$serverName = "";
+// Datos de conexión comunes
+$host = "localhost";
+$user = "root";
+$password = "root";
 
-// Conexión  a la tabla (datos de trazabilidad, OF, etc.)
-$connectionExtras = [
-    "Database" => "Nombre de la bd",
-    "Uid" => "username",
-    "PWD" => "password",
-    "Encrypt" => "no",
-    "TrustServerCertificate" => "yes"
-];
-$connExtras = sqlsrv_connect($serverName, $connectionExtras);
-
+// Conexión a la base de datos de trazabilidad
+$databaseExtras = "bd_trazabilidad";
+$connExtras = mysqli_connect($host, $user, $password, $databaseExtras);
 if (!$connExtras) {
-    die(" Error conectando a DHV_EXTRAS_TEST: " . print_r(sqlsrv_errors(), true));
+    die("Error conectando a $databaseExtras: " . mysqli_connect_error());
 }
 
-// Conexión a  (tabla OHEM para login)
-$connectionUsuarios = [
-    "Database" => "bd",
-    "Uid" => "username",
-    "PWD" => "password",
-    "Encrypt" => "no",
-    "TrustServerCertificate" => "yes"
-];
-$connUsuarios = sqlsrv_connect($serverName, $connectionUsuarios);
-
+// Conexión a la base de datos de usuarios (por ejemplo, para login)
+$databaseUsuarios = "bd_usuarios";
+$connUsuarios = mysqli_connect($host, $user, $password, $databaseUsuarios);
 if (!$connUsuarios) {
-    die("Error conectando de conexion: " . print_r(sqlsrv_errors(), true));
+    die("Error conectando a $databaseUsuarios: " . mysqli_connect_error());
 }
 
-//  Esta es la conexión principal que usarán los archivos normales
+// Esta es la conexión principal que usarán los archivos normales
 $conn = $connExtras;
 ?>
